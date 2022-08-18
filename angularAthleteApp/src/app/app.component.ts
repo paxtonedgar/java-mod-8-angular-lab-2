d7481945ac8b920e353f38e76f81f54e27369764
Paxton Edgar <paxtonedgar3@gmail.com>
2022-08-18 12:22:34 -0400

    init push

--- angularAthleteApp/src/app/app.component.ts
+++ angularAthleteApp/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angularAthleteApp';
+  newName = "";
+  newSport = "";
+  newIsActive = false;
+
   athleteArray: Athlete[] = [
     { name: "Michael Jordan",
       sport: "Basketball",
@@ -23,8 +29,18 @@ export class AppComponent {
       isActive: false
     }
   ];
+
+  createAthlete(){
+    let athlete:Athlete = {
+      name: this.newName,
+      sport: this.newSport,
+      isActive: this.newIsActive
+    }
+    this.athleteArray.push(athlete);
+  }
   
 }
+
 
 interface Athlete{
   name: string;
